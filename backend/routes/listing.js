1187ebbf317551c3293991d47829ea980cd67105
Roshan <roshankawale43@gmail.com>
2024-01-06 19:01:05 +0530

    search aapi

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createListing, deleteListing, getListing, updateListing } from "../controller/listing.js";
+import { createListing, deleteListing, getListing, getListings, updateListing } from "../controller/listing.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/create" , verifyToken , createListing).delete('/delete/:id' , verifyToken , deleteListing).post('/update/:id', verifyToken, updateListing).get('/get/:id', getListing);
+router.post("/create" , verifyToken , createListing).delete('/delete/:id' , verifyToken , deleteListing).post('/update/:id', verifyToken, updateListing).get('/get/:id', getListing).get('/get' , getListings);
 
 export default router;

0e000fff52c7ed2034e134002fb12694729228cb
Roshan <roshankawale43@gmail.com>
2024-01-06 23:09:04 +0530

    update user.js in controller

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,7 +2,6 @@ import User from "../model/user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
-import { response } from "express";
 
 export const signup = async (req, res, next) => {
   try {
@@ -23,7 +22,7 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWt_SECRET_KEY);
     const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })

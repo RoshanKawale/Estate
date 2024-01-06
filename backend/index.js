f708b24c70f7e6c506f6c8ff632372548361720a
Roshan <roshankawale43@gmail.com>
2024-01-06 22:29:57 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(3000, () => {
